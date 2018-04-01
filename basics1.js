0e91137a7ea16c89ffcdef3ee4d73fbd65682032
firezdog@yahoo.com <firezdog@yahoo.com>
2018-04-01 11:50:59 -0500

    Basic 1

---
+++ basics1.js
@@ -0,0 +1,55 @@
+// Basic 1
+// var x = [];
+// console.log(x);
+// x.push("Coding");
+// x.push("Dojo");
+// x.push("rocks");
+// x.pop();
+// console.log(x) 
+// // Should be ["Coding", "Dojo"]
+
+// // Basic 2
+// const y = []
+// console.log(y) //should be []
+// y.push(88) // You can push into a constant array but you can't change the base value.
+// console.log(y)
+// var z = [88]
+// y = z //This won't work.
+
+// Basic 3
+
+// var z = [9, 10, 6, 5, -1, 20, 13, 2];
+// for (i in z) {
+//     console.log(z[i]);
+// }
+
+// console.log("==================");
+
+// for (i in z) {
+//     if (i == z.length -1) {
+//         continue;
+//     } else {
+//         console.log(z[i]);
+//     }
+// }
+
+// Basic 4
+
+// var names = ['Kadie', 'Joe', 'Fritz', 'Pierre', 'Alphonso'];
+// for (i in names) {
+//     console.log(names[i].length);
+// }
+// for (i in names) {
+//     if (names[i].length == 5) {
+//         console.log(names[i]);
+//     }
+// }
+
+// Basic 5
+
+// function yell(str) {
+//     console.log(str.toUpperCase());
+// }
+
+// yell("hiya");
+
