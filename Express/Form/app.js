36b0a5be4f808760dd05e9924737772329ad2512
firezdog@yahoo.com <firezdog@yahoo.com>
2018-04-05 18:29:42 -0500

    added express projects

---
+++ Express/Form/app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var path = require("path");
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(session({secret: '...'}))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "./static")));
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render("index");
+});
+
+app.post('/submit', function(req,res) {
+    req.session.body = req.body.body;
+    res.redirect('/display')
+});
+
+app.get('/display', function(req, res) {
+    context = {body: req.session.body}
+    res.render("display", context);
+});
+
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+});
--- Node/contentModule/content.js
+++ Node/contentModule/content.js
@@ -1,16 +1,26 @@
 module.exports = function(req, res) {
 
     if (req.url.indexOf('stylesheet')>-1) {
+        
         fs.readFile(`./${req.url}`, 'utf8', function(errors, contents){
-            res.writeHead(200, {'Content-type': 'text/css'});
-            res.write(contents);
-            res.end(); 
+            if (errors) {
+                console.log(errors);
+            } else {
+                res.writeHead(200, {'Content-type': 'text/css'});
+                res.write(contents);
+                res.end();
+            } 
         });
     } else if (req.url.indexOf('image')>-1) {
+        
         fs.readFile(`./${req.url}`, function(errors, contents){
-            res.writeHead(200, {'Content-type': 'img/jpg'});
-            res.write(contents);
-            res.end(); 
+            if (errors) {
+                console.log(errors);
+            } else {
+                res.writeHead(200, {'Content-type': 'img/jpg'});
+                res.write(contents);
+                res.end();
+            } 
         });
     }
     else {
