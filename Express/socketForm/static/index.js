ad18759592d92fe8b0d1aa31b4a05c7507d0eb6d
firezdog@yahoo.com <firezdog@yahoo.com>
2018-04-07 12:25:30 -0500

    added socket capable form

---
+++ Express/socketForm/static/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var socket = io.connect();
+    socket.on('updated_message', function(form) {
+        $('.card').append(`<div>You emitted the following information to the server: Name - ${form.name}, Dojo - ${form.dojo}, Favorite Language - ${form.language}, Comment - ${form.comment}</div><hr>`);
+    });
+    socket.on('random_number', function(number) {
+        $('.card').append(`<div>Your lucky number emitted by the server is ${number.random}.</div>`);
+    });
+    $('form').submit(function(e){
+        $('.card').html('');
+        e.preventDefault();
+        var data = $(this).serialize();
+        socket.emit("submit", data);
+    });
+});
