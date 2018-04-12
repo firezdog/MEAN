c3282ad48e1098840fcb5decfad71d52cfc914b1
firezdog@yahoo.com <firezdog@yahoo.com>
2018-04-12 15:44:00 -0500

    display tasks

--- Angular/angularTasks/angularTasksApp/src/app/app.component.ts
+++ Angular/angularTasks/angularTasksApp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
@@ -6,7 +6,33 @@ import { HttpService } from './http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  num: number;
+  randNum: number;
+  str: string;
+  first_name: string;
+
+  tasks;
+  task;
   title = 'angularTaskApp';
+  
   constructor(private _httpService: HttpService){};
+  ngOnInit(){
+    // this.num = 7;
+    // this.randNum = Math.floor(1 + Math.random()*2);
+    // this.str = "Hello Angular Developer";
+    // this.first_name = "Alex";
+    this.getTasksFromService();
+  }
+  getTasksFromService(){
+    let all_tasks = this._httpService.getTasks();
+    all_tasks.subscribe(data => {
+      // console.log("Got our tasks", data);
+      this.tasks = data;
+    });
+    let one_task = this._httpService.getOneTask();
+    one_task.subscribe(data => {
+      this.task = data;
+    });
+  }
 }
--- Angular/angularTasks/angularTasksApp/src/app/http.service.ts
+++ Angular/angularTasks/angularTasksApp/src/app/http.service.ts
@@ -5,15 +5,43 @@ import { HttpClient } from '@angular/common/http';
 export class HttpService {
 
   constructor(private _http: HttpClient) {
-    this.getTasks();
-    this.getOneTask();
+    // this.getTasks();
+    // this.getOneTask();
+    // this.getPokemon();
   }
   getTasks() {
-    let allTasksObservable = this._http.get('/tasks');
-    allTasksObservable.subscribe(data => console.log("Got our tasks!", data));
+    // let allTasksObservable = this._http.get('/tasks');
+    // allTasksObservable.subscribe(data => console.log("Got our tasks!", data));
+    return this._http.get('/tasks');
   }
   getOneTask() {
-    let oneTaskObservable = this._http.get('/tasks/5acf8eba1c66de0174ecbf1c');
-    oneTaskObservable.subscribe(data => console.log("Got the task!", data));
+    // let oneTaskObservable = this._http.get('/tasks/5acf8eba1c66de0174ecbf1c');
+    // oneTaskObservable.subscribe(data => console.log("Got the task!", data))
+    return this._http.get('/tasks/5acf8eba1c66de0174ecbf1c')
   }
+  // getPokemon() {
+  //   let _this = this;
+  //   let pokemonObservable = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
+  //   pokemonObservable.subscribe(data => {
+  //     let chloro = data.abilities[0].ability.name;
+  //     let overgrow = data.abilities[1].ability.name
+  //     console.log(`Bulbasaur's abilities are ${chloro} and ${overgrow}.`);
+  //     let sharing = _this._http.get(`https://pokeapi.co/api/v2/ability/${chloro}/`);
+  //     sharing.subscribe(data => {
+  //       console.log(`${data.pokemon.length} pokemon share the ${chloro} ability. They are as follows:`)
+  //       for (let pokemon of data.pokemon) {
+  //         let name = pokemon.pokemon.name
+  //         let otherAbilities = _this._http.get(`https://pokeapi.co/api/v2/pokemon/${name}/`);
+  //         otherAbilities.subscribe(data => {
+  //           console.log(`${name}, whose other abilities include...`);
+  //           for (let ability of data.abilities) {
+  //             if (ability.ability.name !== chloro) {
+  //               console.log(ability.ability.name);
+  //             }
+  //           };
+  //         });
+  //       }
+  //     });
+  //   });
+  // }
 }
